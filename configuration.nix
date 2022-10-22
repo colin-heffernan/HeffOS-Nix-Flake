610f604a7b1eb6968dca57bda3c473b64fa91638
Colin Heffernan <colinpheffernan@gmail.com>
2022-10-22 18:58:27 -0400

    Upgrade kernel version

--- configuration.nix
+++ configuration.nix
@@ -63,7 +63,7 @@ in
 	boot.loader.efi.canTouchEfiVariables = true;
 	
 	# Kernel stuff
-	boot.kernelPackages = pkgs.linuxKernel.packages.linux_5_19;
+	boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_0;
 	boot.initrd.kernelModules = [ "amdgpu" ];
 	boot.extraModulePackages = with config.boot.kernelPackages; [
 		v4l2loopback
