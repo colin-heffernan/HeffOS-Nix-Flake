2d0bbdc3b9bb4518dd1bda3fead8a0be8b7e765f
Colin Heffernan <colinpheffernan@gmail.com>
2023-06-22 15:55:28 -0400

    Update Helix config

--- home.nix
+++ home.nix
@@ -97,9 +97,81 @@
   };
 
   # Helix config
-  home.file.".config/helix" = {
-    source = ./dotfiles/.config/helix;
-    recursive = true;
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    languages = {
+      language-server.rust-analyzer = {
+        config = {
+          check.command = "clippy";
+        };
+      };
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+          formatter = {
+            command = "alejandra";
+            args = ["--quiet"];
+          };
+        }
+      ];
+    };
+    settings = {
+      theme = "catppuccin_mocha_nobg";
+      editor = {
+        mouse = false;
+        middle-click-paste = false;
+        idle-timeout = 300;
+        completion-replace = true;
+        true-color = true;
+        undercurl = true;
+        color-modes = true;
+        statusline = {
+          left = ["mode"];
+          center = [];
+          right = [];
+          mode = {
+            normal = "NORMAL";
+            insert = "INSERT";
+            select = "SELECT";
+          };
+        };
+        lsp = {
+          display-messages = true;
+        };
+        cursor-shape = {
+          insert = "bar";
+        };
+        file-picker = {
+          hidden = false;
+          max-depth = 5;
+        };
+        auto-pairs = {
+          "(" = ")";
+          "{" = "}";
+          "[" = "]";
+          "'" = "'";
+          "\"" = "\"";
+          "`" = "`";
+          "<" = ">";
+        };
+        whitespace = {
+          render = "all";
+        };
+        indent-guides = {
+          render = true;
+        };
+      };
+    };
+    themes = {
+      catppuccin_mocha_nobg = {
+        inherits = "catppuccin_mocha";
+        "ui.background" = {
+          fg = "foreground";
+        };
+      };
+    };
   };
 
   # Hyprland config
