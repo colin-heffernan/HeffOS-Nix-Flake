9e92d1785a3a0ce2905c3481b5c688a9a16c3a7c
Colin Heffernan <colinpheffernan@gmail.com>
2024-12-08 22:33:24 -0500

    Allow for default editor selection

--- modules/nixos/programs/editors/default.nix
+++ modules/nixos/programs/editors/default.nix
@@ -1,6 +1,12 @@
-{
+{lib, ...}: {
   imports = [
     ./helix.nix
     ./neovim.nix
   ];
+
+  options.heffos.editors.default = lib.mkOption {
+    type = lib.types.str;
+    description = "Which text editor to use by default.";
+    default = null;
+  };
 }
