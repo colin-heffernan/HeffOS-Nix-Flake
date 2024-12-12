63d2d71e6c6bd7622f5f0ebe626a0d6d176b34e4
Colin Heffernan <colinpheffernan@gmail.com>
2024-12-11 19:21:08 -0500

    Remove multiplexer binds from Kitty

--- modules/home-manager/programs/terminal-emulators/kitty.nix
+++ modules/home-manager/programs/terminal-emulators/kitty.nix
@@ -13,12 +13,6 @@
       keybindings = {
         "ctrl+shift+c" = "copy_to_clipboard";
         "ctrl+shift+v" = "paste_from_clipboard";
-        "alt+t" = "launch --cwd=current --location=split";
-        "alt+c" = "close_window";
-        "alt+left" = "neighboring_window left";
-        "alt+down" = "neighboring_window down";
-        "alt+up" = "neighboring_window up";
-        "alt+right" = "neighboring_window right";
       };
       settings = {
         clear_all_shortcuts = "yes";
