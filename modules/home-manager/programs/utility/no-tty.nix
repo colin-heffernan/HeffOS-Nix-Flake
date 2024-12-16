7023d0621ce3f49319eb4c25a8d7ff99257b96c6
Colin Heffernan <colinpheffernan@gmail.com>
2024-12-15 23:22:55 -0500

    Uninstall Eza, LF, and Pistol

--- modules/home-manager/programs/utility/no-tty.nix
+++ modules/home-manager/programs/utility/no-tty.nix
@@ -5,31 +5,6 @@
   ...
 }: {
   config = lib.mkIf osConfig.heffos.utility.no-tty.enable {
-    home.file.".config/lf" = {
-      source = ./lf;
-      recursive = true;
-    };
-
-    programs.pistol = {
-      enable = true;
-      associations = [
-        {
-          mime = "text/*";
-          command = "cat %pistol-filename%";
-        }
-      ];
-    };
-
-    programs.eza = {
-      enable = true;
-      colors = "always";
-      extraOptions = [
-        "-la"
-        "--group-directories-first"
-      ];
-      icons = "always";
-    };
-
     programs.yazi = {
       enable = true;
       flavors.catppuccin-mocha = inputs.yazi-flavors + "/catppuccin-mocha.yazi";
--- modules/nixos/programs/utility/no-tty.nix
+++ modules/nixos/programs/utility/no-tty.nix
@@ -8,10 +8,7 @@
 
   config = lib.mkIf config.heffos.utility.no-tty.enable {
     environment.systemPackages = with pkgs; [
-      eza
       lazygit
-      lf
-      pistol
       yazi
       fzf
       poppler
