27ed235f43188744ad9377a0b942fe4bb7c700b2
Colin Heffernan <colinpheffernan@gmail.com>
2024-12-20 02:51:28 -0500

    Enable support for GIMP plugins

--- modules/nixos/programs/utility/multimedia.nix
+++ modules/nixos/programs/utility/multimedia.nix
@@ -6,6 +6,11 @@
 }: {
   options.heffos.utility.multimedia = {
     enable = lib.mkEnableOption "multimedia utilities";
+    gimpPlugins = lib.mkOption {
+      type = lib.types.listOf lib.types.package;
+      description = "List of GIMP plugins to install.";
+      default = [];
+    };
     obsPlugins = lib.mkOption {
       type = lib.types.listOf lib.types.package;
       description = "List of OBS plugins to install.";
@@ -15,7 +20,9 @@
 
   config = lib.mkIf config.heffos.utility.multimedia.enable {
     environment.systemPackages = with pkgs; [
-      gimp
+      (gimp-with-plugins.override {
+        plugins = config.heffos.utility.multimedia.gimpPlugins;
+      })
       inkscape
       (wrapOBS {
         plugins = config.heffos.utility.multimedia.obsPlugins;
