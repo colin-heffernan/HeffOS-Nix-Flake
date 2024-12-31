52169442d4fa963b64f8b6205e354be4c243339c
Colin Heffernan <colinpheffernan@gmail.com>
2024-12-30 21:13:55 -0500

    Disable ROCm

--- modules/nixos/system/hardware-acceleration.nix
+++ modules/nixos/system/hardware-acceleration.nix
@@ -1,7 +1,7 @@
 {
   config,
   lib,
-  pkgs,
+  # pkgs,
   ...
 }: {
   options.heffos.system.hardware-acceleration.enable = lib.mkEnableOption "hardware acceleration";
@@ -10,9 +10,9 @@
     hardware.graphics = {
       enable = true;
       enable32Bit = true;
-      extraPackages = with pkgs; [
-        rocmPackages.clr.icd
-      ];
+      # extraPackages = with pkgs; [
+      #   rocmPackages.clr.icd
+      # ];
     };
   };
 }
