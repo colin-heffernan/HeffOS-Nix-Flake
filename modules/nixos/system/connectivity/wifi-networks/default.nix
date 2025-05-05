71eb97d353cdaea2c29acb83e5eae2f932c98900
Colin Heffernan <colinpheffernan@gmail.com>
2025-05-05 02:17:11 -0400

    Remove old network

--- modules/nixos/system/connectivity/wifi-networks/default.nix
+++ modules/nixos/system/connectivity/wifi-networks/default.nix
@@ -5,10 +5,9 @@
 }: {
   imports = [
     ./a12.nix
-    ./eduroam.nix
   ];
 
-  config = lib.mkIf (config.heffos.system.connectivity.wifi-networks.a12.enable || config.heffos.system.connectivity.wifi-networks.eduroam.enable) {
+  config = lib.mkIf (config.heffos.system.connectivity.wifi-networks.a12.enable) {
     networking.wireless = {
       enable = true;
       secretsFile = "/home/colin/Repos/HeffOS-Nix-Flake/secrets/wireless.conf";
