d9adc85ff83fe285034744dd62ef0c5419f97065
Colin Heffernan <colinpheffernan@gmail.com>
2025-06-09 11:39:32 -0400

    Install MPC alongside MPD

--- modules/nixos/services/mpd.nix
+++ modules/nixos/services/mpd.nix
@@ -1,3 +1,14 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   options.heffos.services.mpd.enable = lib.mkEnableOption "Music Player Daemon";
+
+  config = lib.mkIf config.heffos.services.mpd.enable {
+    environment.systemPackages = with pkgs; [
+      mpc
+    ];
+  };
 }
