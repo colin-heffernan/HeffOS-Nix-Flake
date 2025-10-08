8f65f6a906f415bf041dd3d600da54b58bea1c15
Colin Heffernan <colinpheffernan@gmail.com>
2025-10-07 20:27:28 -0400

    Bump stable to 25.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-dev.url = "github:colin-heffernan/nixpkgs";
 
     # Home Manager
