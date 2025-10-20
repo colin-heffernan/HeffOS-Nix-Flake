c77196ba301e2277502a1b26279e8e20ad8a8ede
Colin Heffernan <colinpheffernan@gmail.com>
2025-10-20 12:38:55 -0400

    Move system and pkgs to per-host (setup for non-x86 hosts)

--- hosts/default.nix
+++ hosts/default.nix
@@ -6,39 +6,46 @@
   flake.nixosConfigurations = let
     inherit (inputs.nixpkgs.lib) nixosSystem;
 
-    system = "x86_64-linux";
-
-    pkgs = import inputs.nixpkgs {
-      inherit system;
-
-      # Allow unfree packages
-      config.allowUnfree = true;
-
-      # Set up overlays
-      overlays = [
-        (final: prev: {
-          # Make `pkgs.stable` point to `nixos/nixpkgs/nixos-$STABLE`
-          stable = import inputs.nixpkgs-stable { system = final.system; config = final.config; };
-
-          # Make `pkgs.dev` point to `colin-heffernan/nixpkgs`
-          dev = import inputs.nixpkgs-dev { system = final.system; config = final.config; };
-
-          # Use the latest Discord tarball
-          discord = prev.discord.overrideAttrs (
-            _: {src = inputs.discord;}
-          );
-        })
-      ];
-    };
-
     specialArgs = {inherit self inputs;};
   in {
     # Main PC
-    heffos-hitori = nixosSystem {
-      inherit pkgs system specialArgs;
-      modules = [
-        ./hitori
-      ];
-    };
+    heffos-hitori = let
+      system = "x86_64-linux";
+
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+
+        # Allow unfree packages
+        config.allowUnfree = true;
+
+        # Set up overlays
+        overlays = [
+          (final: prev: {
+            # Make `pkgs.stable` point to `nixos/nixpkgs/nixos-$STABLE`
+            stable = import inputs.nixpkgs-stable {
+              system = final.system;
+              config = final.config;
+            };
+
+            # Make `pkgs.dev` point to `colin-heffernan/nixpkgs`
+            dev = import inputs.nixpkgs-dev {
+              system = final.system;
+              config = final.config;
+            };
+
+            # Use the latest Discord tarball
+            discord = prev.discord.overrideAttrs (
+              _: {src = inputs.discord;}
+            );
+          })
+        ];
+      };
+    in
+      nixosSystem {
+        inherit pkgs system specialArgs;
+        modules = [
+          ./hitori
+        ];
+      };
   };
 }
