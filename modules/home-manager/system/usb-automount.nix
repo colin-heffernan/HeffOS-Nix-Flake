ee57482740b8999b7b6eb5229ba2622ca78dcf65
Colin Heffernan <colinpheffernan@gmail.com>
2025-10-20 16:31:43 -0400

    Install udiskie utils for unmounting

--- modules/home-manager/system/usb-automount.nix
+++ modules/home-manager/system/usb-automount.nix
@@ -1,9 +1,14 @@
 {
   lib,
   osConfig,
+  pkgs,
   ...
 }: {
   config = lib.mkIf osConfig.heffos.system.usb-automount.enable {
     services.udiskie.enable = true;
+
+    home.packages = with pkgs; [
+      udiskie
+    ];
   };
 }
