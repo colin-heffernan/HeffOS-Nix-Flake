fd56ebe28ece1308fe23def14233c43b792543d0
Colin Heffernan <colinpheffernan@gmail.com>
2025-10-20 11:20:17 -0400

    Use non-module Obsidian

--- modules/home-manager/utility/obsidian.nix
+++ modules/home-manager/utility/obsidian.nix
@@ -1,11 +1,15 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   options.heffos-home.utility.obsidian.enable = lib.mkEnableOption "Obsidian";
 
   config = lib.mkIf config.heffos-home.utility.obsidian.enable {
-    programs.obsidian.enable = true;
+    # programs.obsidian.enable = true;
+    home.packages = with pkgs; [
+      obsidian
+    ];
   };
 }
