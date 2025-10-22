40e784deb6d458a5d111c4e417f108c0203a0635
Colin Heffernan <colinpheffernan@gmail.com>
2025-10-22 18:56:50 +0000

    Add enable/disable for bootloader

--- modules/nixos/system/bootloader/default.nix
+++ modules/nixos/system/bootloader/default.nix
@@ -8,7 +8,9 @@
     ./rpi.nix
   ];
 
-  config = lib.mkIf (!config.heffos.system.bootloader.dualboot.enable && !config.heffos.system.bootloader.rpi.enable) {
+  options.heffos.system.bootloader.enable = lib.mkEnableOption "a bootloader";
+
+  config = lib.mkIf (config.heffos.system.bootloader.enable && !config.heffos.system.bootloader.dualboot.enable && !config.heffos.system.bootloader.rpi.enable) {
     boot.loader.systemd-boot.enable = true;
   };
 }
