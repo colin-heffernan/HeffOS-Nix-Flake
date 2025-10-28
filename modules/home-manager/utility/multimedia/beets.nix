e78681e14864eb3faf9862989121998760de0980
Colin Heffernan <colinpheffernan@gmail.com>
2025-10-28 14:43:43 -0400

    Add Beets plugins

--- modules/home-manager/utility/multimedia/beets.nix
+++ modules/home-manager/utility/multimedia/beets.nix
@@ -6,9 +6,21 @@
   config = lib.mkIf config.heffos-home.utility.multimedia.enable {
     programs.beets = {
       enable = true;
+      mpdIntegration.enableUpdate = true;
       settings = {
         directory = "${config.home.homeDirectory}/Music";
         library = "${config.home.homeDirectory}/beets.db";
+        plugins = [
+          "fetchart"
+          "embedart"
+          "convert"
+        ];
+        fetchart = {
+          auto = "yes";
+        };
+        embedart = {
+          auto = "yes";
+        };
       };
     };
   };
