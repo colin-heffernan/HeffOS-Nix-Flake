d73be31254f7ad74891def5ff89be68cb98be444
Colin Heffernan <colinpheffernan@gmail.com>
2025-11-21 12:25:38 -0500

    Install a cursor theme (Posy's cursors)

--- modules/nixos/environments/compositors/niri.nix
+++ modules/nixos/environments/compositors/niri.nix
@@ -8,13 +8,22 @@
 
   config = lib.mkIf config.heffos.environments.compositors.niri.enable {
     programs.niri.enable = true;
-    
+
     # Prevent GNOME's SSH agent from interfering with `ssh-agent`
     services.gnome.gcr-ssh-agent.enable = false;
+
+    # Set icons
+    xdg.icons = {
+      enable = true;
+      fallbackCursorThemes = lib.mkDefault [
+        "Posy_Cursor_Black"
+      ];
+    };
 
     environment.systemPackages = with pkgs; [
       xwayland-satellite
       wl-clipboard
+      posy-cursors
     ];
   };
 }
