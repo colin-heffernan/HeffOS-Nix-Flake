98620a848b69b5fe736aadd6790f6214c014d9fe
Colin Heffernan <colinpheffernan@gmail.com>
2025-12-04 14:10:13 -0500

    Add Dust to modern alts

--- modules/home-manager/utility/modern-alts/default.nix
+++ modules/home-manager/utility/modern-alts/default.nix
@@ -1,8 +1,19 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ./bat.nix
     ./eza.nix
   ];
 
   options.heffos-home.utility.modern-alts.enable = lib.mkEnableOption "modern alternatives to standard utilities";
+
+  config = lib.mkIf config.heffos-home.utility.modern-alts.enable {
+    home.packages = with pkgs; [
+      dust
+    ];
+  };
 }
