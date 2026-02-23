c08c2ae9c9b43d4ab6a83b6c8e431912e737d250
Colin Heffernan <colinpheffernan@gmail.com>
2026-02-22 20:23:12 -0500

    Update NixOS stable to 25.11

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
     # Home Manager
     home-manager = {
