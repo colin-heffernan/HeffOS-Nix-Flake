ea3c81038484aeec28fb1fba9403d895a1492b3b
Colin Heffernan <colinpheffernan@gmail.com>
2026-03-25 13:21:26 -0400

    Disable Stoat until it bumps Electron

--- hosts/hitori/default.nix
+++ hosts/hitori/default.nix
@@ -188,7 +188,7 @@
         communication = {
           discord.enable = true;
           element.enable = true;
-          stoat.enable = true;
+          # stoat.enable = true;
           zoom.enable = true;
         };
         editors = {
